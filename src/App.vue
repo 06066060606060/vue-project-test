2b37dae264f91ee801882c55fb4bf2b9719a4b76
06066060606060 <xmicky+git@hotmail.fr>
2022-07-19 23:14:56 +0400

    1st commit clean project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import ContentHome from './components/ContentHome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <div class="home">
+    <ContentHome msg2="ok test" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+.home {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+
+</style>
---
+++ src/components/ContentHome.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  msg2: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <div class="thehome">
+       <h1>{{ msg2 }}</h1>
+    </div>
+</template>
+
+<style scoped>
+.thehome h1 {
+  text-align: center;
+  color:whitesmoke;
+}
+</style>
